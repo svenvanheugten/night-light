6baf7257659a976c3a3d28361c13a5183fbf25b5
Sven van Heugten <sven@memcmp.org>
2026-01-04 10:31:13 +0100

    Put Arbitraries on the entire test class

--- NightLight.Core.Tests/NightLightTests.fs
+++ NightLight.Core.Tests/NightLightTests.fs
@@ -1,14 +1,16 @@
-module NightLight.Core.Tests.NightLightTests
+namespace NightLight.Core.Tests
 
 open FsCheck.Xunit
 
-[<Property(Arbitrary = [| typeof<Arbitraries> |])>]
-let ``Brightness should always be under 255`` (fakeHome: FakeHome) (interactions: Interaction list) =
-    interactions |> Seq.iter (fun interaction -> fakeHome.Interact interaction)
+[<Properties(Arbitrary = [| typeof<Arbitraries> |])>]
+type NightLightTests() =
+    [<Property>]
+    let ``Brightness should always be under 255`` (fakeHome: FakeHome) (interactions: Interaction list) =
+        interactions |> Seq.iter (fun interaction -> fakeHome.Interact interaction)
 
-    fakeHome.LightStates
-    |> Seq.choose (fun (_, state) ->
-        match state with
-        | On brightness -> Some brightness
-        | Off -> None)
-    |> Seq.forall (fun brightness -> brightness < 255uy)
+        fakeHome.LightStates
+        |> Seq.choose (fun (_, state) ->
+            match state with
+            | On brightness -> Some brightness
+            | Off -> None)
+        |> Seq.forall (fun brightness -> brightness < 255uy)
