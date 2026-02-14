a7d27d91b4c683c59bad77f8ca5c65034d3e61c6
Sven van Heugten <sven@memcmp.org>
2026-02-14 19:36:36 +0100

    Add night-light to the module

--- module.nix
+++ module.nix
@@ -1,12 +1,14 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 
 with lib;
 let
   cfg = config.services.nightLight;
+  pkg = pkgs.callPackage ./default.nix { };
 in
 {
   options.services.nightLight = {
@@ -15,5 +17,23 @@ in
 
   config = mkIf cfg.enable {
     services.zigbee2mqtt.enable = true;
+    users.groups.night-light = { };
+    users.users.night-light = {
+      isSystemUser = true;
+      description = "Night Light";
+      group = "night-light";
+    };
+    systemd.services.night-light = {
+      description = "Night Light";
+      wantedBy = [ "multi-user.target" ];
+      after = [ "network.target" ];
+
+      serviceConfig = {
+        ExecStart = "${pkg}/bin/NightLight";
+        Restart = "on-failure";
+        User = "night-light";
+        Group = "night-light";
+      };
+    };
   };
 }
