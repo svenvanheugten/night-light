eedce14df40cdb66c6b60dfcbae958fe71386b81
Sven van Heugten <sven@memcmp.org>
2026-02-22 20:26:18 +0100

    Add git-check-assertions

--- flake.nix
+++ flake.nix
@@ -2,12 +2,18 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    git-check-assertions = {
+      url = "git+https://codeberg.org/svenvanheugten/git-check-assertions.git?ref=main";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
   };
   outputs =
     {
       self,
       nixpkgs,
       flake-utils,
+      git-check-assertions,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
@@ -17,7 +23,10 @@
       {
         packages.default = pkgs.callPackage ./default.nix { };
         devShells.default = pkgs.mkShell {
-          packages = [ pkgs.dotnet-sdk_10 ];
+          packages = [
+            pkgs.dotnet-sdk_10
+            git-check-assertions.packages.${system}.default
+          ];
         };
       }
     )
